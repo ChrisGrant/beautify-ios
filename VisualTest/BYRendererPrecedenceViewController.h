42007ce9e40e5e94bb83eb8c7581d88ae20b1000
Chris Grant <cgrant@scottlogic.co.uk>
2013-10-21 12:38:07 +0100

    Added a renderer precedence view controller.

---
+++ VisualTest/BYRendererPrecedenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  BYRendererPrecedenceViewController.h
+//  Beautify
+//
+//  Created by Chris Grant on 21/10/2013.
+//  Copyright (c) 2013 Beautify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYRendererPrecedenceViewController : UIViewController
+
+@end
