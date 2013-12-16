6914f48624693ad95071928ce433bb94b3efaa62
Chris Grant <cgrant@scottlogic.co.uk>
2013-12-16 15:12:54 +0000

    Added backgroundImage to the navigation bar. It’s that simple!

--- Beautify/BYNavigationBarStyle.h
+++ Beautify/BYNavigationBarStyle.h
@@ -12,13 +12,14 @@
 #import "BYText.h"
 #import "BYDropShadow.h"
 
-@class BYText, BYTextShadow, BYGradient;
+@class BYText, BYTextShadow, BYGradient, BYBackgroundImage;
 
 @interface BYNavigationBarStyle : BYViewStyle
 
 @property UIColor<Optional> *tintColor;
 @property UIColor<Optional> *backgroundColor;
 @property BYGradient<Optional> *backgroundGradient;
+@property BYBackgroundImage<Optional> *backgroundImage;
 
 @property BYText *title;
 @property BYTextShadow<Optional> *titleShadow;
